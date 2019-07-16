7925f1699e83a5812eccda3ef3a2ccd123a24894
sgoparapu <34513908+sgoparapu@users.noreply.github.com>
2019-07-16 16:18:58 +0530

    Delete web.sh

--- web.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo hostnamectl set-hostname web.tw
-sudo amazon-linux-extras install ansible2
-sudo yum install git -y
-git clone https://github.com/sgoparapu/ansible-tw.git
-cd /home/ec2-user/ansible-tw
-sudo ansible-playbook -l localhost httpd.yml
-sudo cp /home/ec2-user/ansible-tw/index.html /var/www/html/
